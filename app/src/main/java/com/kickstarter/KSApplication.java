e9168e6eedcef405a08300424656e2c086d9a791
pavelfedor <pavelbjorn@gmail.com>
2018-05-16 11:07:48 +0300

    Implement AppSpector

--- app/src/main/java/com/kickstarter/KSApplication.java
+++ app/src/main/java/com/kickstarter/KSApplication.java
@@ -1,12 +1,16 @@
 package com.kickstarter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 import android.text.TextUtils;
+import android.util.Log;
 
+import com.appspector.sdk.AppSpector;
 import com.facebook.FacebookSdk;
 import com.google.android.gms.iid.InstanceID;
 import com.kickstarter.libs.ApiCapabilities;
@@ -35,86 +39,103 @@ import javax.inject.Inject;
 import timber.log.Timber;
 
 public class KSApplication extends MultiDexApplication {
-  private ApplicationComponent component;
-  private RefWatcher refWatcher;
-  @Inject protected CookieManager cookieManager;
-  @Inject protected PushNotifications pushNotifications;
+    private ApplicationComponent component;
+    private RefWatcher refWatcher;
+    @Inject
+    protected CookieManager cookieManager;
+    @Inject
+    protected PushNotifications pushNotifications;
 
-  @Override
-  @CallSuper
-  public void onCreate() {
-    super.onCreate();
+    @Override
+    @CallSuper
+    public void onCreate() {
+        super.onCreate();
 
-    // Send crash reports in release builds
-    if (!BuildConfig.DEBUG && !isInUnitTests()) {
-      checkForCrashes();
+        // Send crash reports in release builds
+        if (!BuildConfig.DEBUG && !isInUnitTests()) {
+            checkForCrashes();
+        }
+
+        MultiDex.install(this);
+
+        if (BuildConfig.DEBUG){
+            initAppSpector();
+        }
+
+        // Only log for internal builds
+        if (BuildConfig.FLAVOR_AUDIENCE.equals("internal")) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        if (!isInUnitTests() && ApiCapabilities.canDetectMemoryLeaks()) {
+            this.refWatcher = LeakCanary.install(this);
+        }
+
+        JodaTimeAndroid.init(this);
+
+        this.component = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+        component().inject(this);
+
+        setVisitorCookie();
+
+        FacebookSdk.sdkInitialize(this);
+
+        this.pushNotifications.initialize();
+
+        final ApplicationLifecycleUtil appUtil = new ApplicationLifecycleUtil(this);
+        registerActivityLifecycleCallbacks(appUtil);
+        registerComponentCallbacks(appUtil);
+
+        //test();
     }
 
-    MultiDex.install(this);
-
-    // Only log for internal builds
-    if (BuildConfig.FLAVOR_AUDIENCE.equals("internal")) {
-      Timber.plant(new Timber.DebugTree());
+    public ApplicationComponent component() {
+        return this.component;
     }
 
-    if (!isInUnitTests() && ApiCapabilities.canDetectMemoryLeaks()) {
-      this.refWatcher = LeakCanary.install(this);
+    public static RefWatcher getRefWatcher(final @NonNull Context context) {
+        final KSApplication application = (KSApplication) context.getApplicationContext();
+        return application.refWatcher;
     }
 
-    JodaTimeAndroid.init(this);
+    public boolean isInUnitTests() {
+        return false;
+    }
 
-    this.component = DaggerApplicationComponent.builder()
-      .applicationModule(new ApplicationModule(this))
-      .build();
-    component().inject(this);
+    private void checkForCrashes() {
+        final String appId = Build.isExternal()
+                ? Secrets.HockeyAppId.EXTERNAL
+                : Secrets.HockeyAppId.INTERNAL;
 
-    setVisitorCookie();
-
-    FacebookSdk.sdkInitialize(this);
-
-    this.pushNotifications.initialize();
-
-    final ApplicationLifecycleUtil appUtil = new ApplicationLifecycleUtil(this);
-    registerActivityLifecycleCallbacks(appUtil);
-    registerComponentCallbacks(appUtil);
-  }
-
-  public ApplicationComponent component() {
-    return this.component;
-  }
-
-  public static RefWatcher getRefWatcher(final @NonNull Context context) {
-    final KSApplication application = (KSApplication) context.getApplicationContext();
-    return application.refWatcher;
-  }
-
-  public boolean isInUnitTests() {
-    return false;
-  }
-
-  private void checkForCrashes() {
-    final String appId = Build.isExternal()
-      ? Secrets.HockeyAppId.EXTERNAL
-      : Secrets.HockeyAppId.INTERNAL;
-
-    CrashManager.register(this, appId, new CrashManagerListener() {
-      public boolean shouldAutoUploadCrashes() {
-        return true;
-      }
-    });
-  }
+        CrashManager.register(this, appId, new CrashManagerListener() {
+            public boolean shouldAutoUploadCrashes() {
+                return true;
+            }
+        });
+    }
 
 
-  private void setVisitorCookie() {
-    final String deviceId = InstanceID.getInstance(this).getId();
-    final String uniqueIdentifier = TextUtils.isEmpty(deviceId) ? UUID.randomUUID().toString() : deviceId;
-    final HttpCookie cookie = new HttpCookie("vis", uniqueIdentifier);
-    cookie.setMaxAge(DateTime.now().plusYears(100).getMillis());
-    cookie.setSecure(true);
-    final URI webUri = URI.create(Secrets.WebEndpoint.PRODUCTION);
-    final URI apiUri = URI.create(ApiEndpoint.PRODUCTION.url());
-    this.cookieManager.getCookieStore().add(webUri, cookie);
-    this.cookieManager.getCookieStore().add(apiUri, cookie);
-    CookieHandler.setDefault(this.cookieManager);
-  }
+    private void setVisitorCookie() {
+        final String deviceId = InstanceID.getInstance(this).getId();
+        final String uniqueIdentifier = TextUtils.isEmpty(deviceId) ? UUID.randomUUID().toString() : deviceId;
+        final HttpCookie cookie = new HttpCookie("vis", uniqueIdentifier);
+        cookie.setMaxAge(DateTime.now().plusYears(100).getMillis());
+        cookie.setSecure(true);
+        final URI webUri = URI.create(Secrets.WebEndpoint.PRODUCTION);
+        final URI apiUri = URI.create(ApiEndpoint.PRODUCTION.url());
+        this.cookieManager.getCookieStore().add(webUri, cookie);
+        this.cookieManager.getCookieStore().add(apiUri, cookie);
+        CookieHandler.setDefault(this.cookieManager);
+    }
+
+    private void initAppSpector() {
+        AppSpector.build(this)
+                .addPerformanceMonitor()
+                .addScreenshotMonitor()
+                .addHttpMonitor()
+                .addLogMonitor()
+                .run("ZDJhODkwYzMtYTMwZS00MDk0LWE0YjMtNDdjYzM5YTVjMWNk");
+    }
 }
